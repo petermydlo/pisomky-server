38e91112ac0ac5f4dd373f74dff3dabb486e1ed5
trinar <mydlop@gmail.com>
2026-04-27 18:36:13 +0200

    Oprav kurzor a klikatelnost na neviditelnych bunkach skupiny

--- pubres/js/overviewtests.js
+++ pubres/js/overviewtests.js
@@ -1,3 +1,7 @@
+document.addEventListener('click', (e) => {
+   if (e.target.closest('.skupina .grid > .neviditelny')) e.stopPropagation();
+}, true);
+
 document.addEventListener('DOMContentLoaded', () => {
 
    function skupinaData(el) {
